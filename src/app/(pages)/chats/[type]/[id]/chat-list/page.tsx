78913758669fae6285b9aad7bc9f5533902ff8ac
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-12-22 22:05:03 -0500

    Refactor ChatListPage: replace authentication handling with useFetchChatsInRealTime hook for real-time chat data fetching

--- src/app/(pages)/chats/[type]/[id]/chat-list/page.tsx
+++ src/app/(pages)/chats/[type]/[id]/chat-list/page.tsx
@@ -2,13 +2,9 @@
 
 import Chat from "@/app/(pages)/chats/[type]/[id]/chat-list/Chat"
 import { TChat } from "@/types"
-import { auth, firestore } from "@/utils/firebase/firebase"
-import useAuthCheck from "@/utils/hooks/useAuthCheck"
-import { collection } from "firebase/firestore"
+import useFetchChatsInRealTime from "@/utils/hooks/fetchData/useFetchChatsInRealTime"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
 import type { JSX } from "react"
-import { useCollection } from "react-firebase-hooks/firestore"
 
 type pageProps = {
   params: {
@@ -18,49 +14,13 @@ type pageProps = {
 }
 
 const ChatListPage = ({ params }: pageProps): JSX.Element => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  // const [isLoading, setIsLoading] = useState(true);
-  const [chats, setChats] = useState<TChat[]>([])
-
   const router = useRouter()
 
-  const [value, loading, error] = useCollection(
-    collection(firestore, "chats"),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    },
-  )
+  const fetchedChat = useFetchChatsInRealTime({ channelId: params.id })
 
-  useAuthCheck(setIsAuthenticated)
-
-  // Authenticate a user
-  useEffect(() => {
-    if (isAuthenticated && value) {
-      const chatList = value.docs.map(
-        (doc) =>
-          ({
-            id: doc.id,
-            ...doc.data(),
-          }) as TChat,
-      )
-
-      setChats(chatList)
-    }
-  }, [isAuthenticated, value, router])
-
-  useEffect(() => {
-    if (loading) {
-      console.log("Loading messages...")
-    }
-
-    if (error) {
-      console.error("Error fetching messages:", error)
-    }
-  }, [loading, error])
-
-  // Local functions ----------------------------------------------------------
   const handleCancelClick = (): void => {
-    if (auth) router.push(`/chats/${params.type}/${params.id}/features`)
+    console.log("handleCancelClick")
+    router.push(`/chats/${params.type}/${params.id}/features`)
   }
 
   return (
@@ -72,7 +32,7 @@ const ChatListPage = ({ params }: pageProps): JSX.Element => {
         </h1>
 
         <div className="flex flex-col gap-3">
-          {chats.map((chat: TChat) => (
+          {fetchedChat?.map((chat: TChat) => (
             <Chat key={chat.id} chat={chat} />
           ))}
         </div>
@@ -83,8 +43,7 @@ const ChatListPage = ({ params }: pageProps): JSX.Element => {
         onClick={handleCancelClick}
         className="w-full rounded-lg bg-white py-4 text-xl font-semibold text-earth-400 shadow transition duration-300 ease-in-out hover:bg-earth-50"
       >
-        {" "}
-        Cancel{" "}
+        Cancel
       </button>
     </div>
   )
