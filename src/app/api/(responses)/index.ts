0d4dd9870961178ce6ff21a28dfb188892ec2f59
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2025-03-13 15:20:56 -0400

    Enhance API response utility functions to accept optional messages for success and error responses, providing default messages when none are specified. Refactor response handling for consistency across various response types.

--- src/app/api/(responses)/index.ts
+++ src/app/api/(responses)/index.ts
@@ -1,6 +1,12 @@
 import { FirebaseFirestore } from "@firebase/firestore-types"
 import { NextResponse } from "next/server"
 
+/**
+ * Returns a 200 success response with an optional message.
+ * If no message is provided, the default message "Success" is used.
+ * @param message - An optional message to include in the success response.
+ * @returns A 200 success response with an optional message or the default message.
+ */
 export const responseFetched = (
   data: FirebaseFirestore.DocumentData | boolean | undefined,
 ): NextResponse =>
@@ -11,30 +17,55 @@ export const responseFetched = (
     { status: 200 },
   )
 
-export const responseUpdated = (type?: "chat" | "user"): NextResponse =>
+/**
+ * Returns a 200 success response with an optional message.
+ * If no message is provided, the default message "Success" is used.
+ * @param message - An optional message to include in the success response.
+ * @returns A 200 success response with an optional message or the default message.
+ */
+export const responseUpdated = (message?: string): NextResponse =>
   NextResponse.json(
     {
-      message: `${type} Updated.`,
+      message: message || "Updated",
     },
     { status: 200 },
   )
 
-export const responseDeleted = (): NextResponse =>
+/**
+ * Returns a 200 success response with an optional message.
+ * If no message is provided, the default message "Success" is used.
+ * @param message - An optional message to include in the success response.
+ * @returns A 200 success response with an optional message or the default message.
+ */
+export const responseDeleted = (message?: string): NextResponse =>
   NextResponse.json(
     {
-      message: "Deleted",
+      message: message || "Deleted",
     },
     { status: 200 },
   )
 
-export const responseSuccess = (): NextResponse =>
+/**
+ * Returns a 200 success response with an optional message.
+ * If no message is provided, the default message "Success" is used.
+ * @param message - An optional message to include in the success response.
+ * @returns A 200 success response with an optional message or the default message.
+ */
+export const responseSuccess = (message?: string): NextResponse =>
   NextResponse.json(
     {
-      message: "Success",
+      message: message || "Success",
     },
     { status: 200 },
   )
 
+/**
+ * Returns a 201 created response with an optional message and id.
+ * @param type - The type of the resource that was created.
+ * @param id - The id of the resource that was created.
+ * @param timestamp - The timestamp of the resource that was created.
+ * @returns A 201 created response with an optional message and id.
+ */
 export const responseCreated = (
   type: "banner" | "chat" | "channel" | "user" | "friend" | "message",
   id?: string,
@@ -49,15 +80,6 @@ export const responseCreated = (
     { status: 201 },
   )
 
-export const responseServerError = (error: unknown): NextResponse => {
-  return NextResponse.json(
-    {
-      error: `Internal Server Error - ${error}`,
-    },
-    { status: 500 },
-  )
-}
-
 /**
  * Returns a 400 error response with an optional message.
  * If no message is provided, the default message "Bad request" is used.
@@ -67,8 +89,16 @@ export const responseServerError = (error: unknown): NextResponse => {
 export const responseBadRequest = (message?: string): NextResponse =>
   NextResponse.json({ error: message }, { status: 400 })
 
+/**
+ * Returns a 401 error response.
+ * @returns A 401 error response.
+ */
 export const responseUnauthorized = NextResponse.json({}, { status: 401 })
 
+/**
+ * Returns a 403 error response.
+ * @returns A 403 error response.
+ */
 export const responseForbidden = NextResponse.json({}, { status: 403 })
 
 /**
@@ -88,4 +118,22 @@ export const responseNotFound = (type?: string): NextResponse => {
   )
 }
 
+/**
+ * Returns a 409 error response.
+ * @returns A 409 error response.
+ */
 export const responseConflict = NextResponse.json({}, { status: 409 })
+
+/**
+ * Returns a 500 error response with an optional error message.
+ * If no message is provided, the default message "Internal Server Error" is used.
+ * @param error - An optional error message to include in the error response.
+ * @returns A 500 error response with an optional error message or the default message.
+ */
+export const responseServerError = (error: unknown): NextResponse =>
+  NextResponse.json(
+    {
+      error: `Internal Server Error - ${error}`,
+    },
+    { status: 500 },
+  )
