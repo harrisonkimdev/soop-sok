ac5128e4630225b7500ddf57cd48492c604e4e9e
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2025-03-14 20:22:37 -0400

    Add AI chat feature to Friends page with navigation to chat bot

--- src/app/(pages)/friends/page.tsx
+++ src/app/(pages)/friends/page.tsx
@@ -5,16 +5,35 @@ import { Friend } from "@/app/(pages)/friends/Friend"
 import { TFriend } from "@/app/types"
 import { auth } from "@/utils/firebase/firebase"
 import useFirebaseHookFriends from "@/utils/hooks/fetchData/useFirebaseHookFriends"
+import { useRouter } from "next/navigation"
 import type { JSX } from "react"
 
 const FriendsPage = (): JSX.Element => {
   const userId = auth.currentUser?.uid || ""
   const friends = useFirebaseHookFriends({ userId })
 
+  const router = useRouter()
+
+  const enterAIChat = (): void => {
+    if (auth && auth.currentUser) {
+      router.push(`/chats/ai-chat/${auth.currentUser.uid}`)
+    }
+  }
+
   return (
     <div className="h-full overflow-y-auto">
       <PageTitle title="Friends" />
       <div className="flex flex-col gap-2">
+        <div
+          onClick={() => enterAIChat()}
+          className="flex gap-4 rounded-lg bg-white p-5 shadow"
+        >
+          <p className="">
+            Are all of your friends busy? <br />
+            Try our AI chat bot today!
+          </p>
+        </div>
+
         {friends && friends.length > 0 ? (
           friends.map((friend: TFriend) => (
             <Friend
