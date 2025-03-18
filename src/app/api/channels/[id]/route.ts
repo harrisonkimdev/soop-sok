a31c486d3eda88af5549207e1070be63b66351ae
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2025-03-18 12:51:54 -0400

    Refactor channel route handler to improve logging and enhance user feedback during channel membership updates. Update action handling for 'enter' and 'leave' actions with clearer console messages and error responses.

--- src/app/api/channels/[id]/route.ts
+++ src/app/api/channels/[id]/route.ts
@@ -11,55 +11,66 @@ export async function PUT(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> },
 ): Promise<NextResponse> {
-  // Dynamic route from the URL
+  // Getting channel id from the URL using dynamic route.
   const cid = (await params).id
-  // Query parameters from the URL
+  // Getting action from the URL query parameters.
   const action = req.nextUrl.searchParams.get("action")
-  // Through the request body
+  // Getting user id from the request body.
   const { uid } = await req.json()
-  console.log(`Channel ID: ${cid}, User ID: ${uid}, Action: ${action}`)
+  console.log(
+    `Backend code running with the given channel ID: ${cid}, User ID: ${uid}, Action: ${action}...`,
+  )
 
+  // Getting the channel document from the firestore collection.
   const channelRef = firestore.collection("channels").doc(cid)
 
   try {
     const channelDoc = await channelRef.get()
     const channelData = channelDoc.data()
-    if (!channelData) return responseNotFound()
     console.log("Channel data:", channelData)
+    if (!channelData) return responseNotFound()
 
+    // Checking if the action is "enter" or "leave".
     if (action === "enter") {
+      // Checking if the user is already in the channel.
       if (channelData.members.includes(uid)) {
         return responseBadRequest("User already in the channel")
       }
 
+      // Adding the user to the channel.
       const newMembers = [...channelData.members, uid]
-      console.log("New members:", newMembers)
-
+      // Updating the number of members in the channel.
       const newNumMembers = channelData.numMembers + 1
-      console.log("New number of members:", newNumMembers)
-
+      console.log(
+        `Updating the channel document with the new members ${newMembers} and number of members ${newNumMembers}...`,
+      )
+      // Updating the channel document with the new members and number of members.
       await channelRef.update({
         members: newMembers,
         numMembers: newNumMembers,
         updatedAt: FieldValue.serverTimestamp(),
       })
-    } else if (action === "leave") {
+    }
+
+    // Checking if the action is "leave".
+    else if (action === "leave") {
+      // Checking if the user is not a member of the channel.
       if (!channelData.members.includes(uid)) {
-        return responseBadRequest("User is not a member of the channel")
+        return responseBadRequest(
+          "Sorry, we lost you in the system. Resetting your session...",
+        )
       }
 
+      // Removing the user from the channel.
       const newMembers = channelData.members.filter(
         (member: string) => member !== uid,
       )
+      // Updating the number of members in the channel.
       const newNumMembers = channelData.numMembers - 1
-      console.log(`New members (${newNumMembers}): ${newMembers}`)
-
-      await channelRef.update({
-        members: newMembers,
-        numMembers: newNumMembers,
-        updatedAt: FieldValue.serverTimestamp(),
-      })
-
+      console.log(
+        `Updating the channel document with the new members ${newMembers} and number of members ${newNumMembers}...`,
+      )
+      // Updating the channel document with the new members and number of members.
       await channelRef.update({
         members: newMembers,
         numMembers: newNumMembers,
