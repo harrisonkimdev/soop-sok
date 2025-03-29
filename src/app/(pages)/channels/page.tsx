e3d71306afbbae2c8876b35dc200638aa19b93cd
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2025-03-29 00:01:10 -0400

    Refactor ChannelPage component to use Firestore for real-time channel data fetching. Replace useFirebaseHookChannels with direct Firestore queries and implement authentication checks. Remove unused useFirebaseHookChannels file.

--- src/app/(pages)/channels/page.tsx
+++ src/app/(pages)/channels/page.tsx
@@ -3,30 +3,60 @@
 import PageTitle from "@/app/(components)/PageTitle"
 import { Channel } from "@/app/(pages)/channels/Channel"
 import { TChannel } from "@/app/types"
-import useFirebaseHookChannels from "@/utils/hooks/fetchData/useFirebaseHookChannels"
+import { firestore } from "@/utils/firebase/firebase"
+import useAuthCheck from "@/utils/hooks/useAuthCheck"
+import {
+  collection,
+  DocumentData,
+  onSnapshot,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore"
 import type { JSX } from "react"
+import { useEffect, useState } from "react"
 
 const ChannelPage = (): JSX.Element => {
-  const fetchedChannels = useFirebaseHookChannels()
+  const [channels, setChannels] = useState<TChannel[]>([])
 
-  if (fetchedChannels) {
-    return (
-      <>
-        <PageTitle title="Channels" />
-        <div className="flex h-full flex-col gap-3 overflow-y-auto">
-          {fetchedChannels.length > 0 ? (
-            fetchedChannels.map((channel: TChannel) => (
-              <Channel key={channel.id} channel={channel} />
-            ))
-          ) : (
-            <p>No channels available.</p>
-          )}
-        </div>
-      </>
+  const isAuthenticated = useAuthCheck()
+
+  useEffect(() => {
+    if (!isAuthenticated) return
+
+    const channelsRef = collection(firestore, "channels")
+    const q = query(channelsRef)
+
+    const unsubscribe = onSnapshot(
+      q,
+      (querySnapshot: QuerySnapshot<DocumentData>) => {
+        const channels = querySnapshot.docs.map(
+          (doc) =>
+            ({
+              id: doc.id,
+              ...doc.data(),
+            }) as TChannel,
+        )
+        setChannels(channels)
+      },
     )
-  } else {
-    return <></>
-  }
+
+    return () => unsubscribe()
+  }, [isAuthenticated])
+
+  return (
+    <>
+      <PageTitle title="Channels" />
+      <div className="flex h-full flex-col gap-3 overflow-y-auto">
+        {channels.length > 0 ? (
+          channels?.map((channel: TChannel) => (
+            <Channel key={channel.id} channel={channel} />
+          ))
+        ) : (
+          <p>No channels available.</p>
+        )}
+      </div>
+    </>
+  )
 }
 
 ChannelPage.displayName = "ChannelPage"
